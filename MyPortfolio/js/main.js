f3878d2b684cfb67590b845909d219a43742a748
Dylan Sheffer <dsheffer16@gmail.com>
2018-02-05 21:40:24 -0500

    added more accessible nav

---
+++ MyPortfolio/js/main.js
@@ -0,0 +1,17 @@
+// Navigation Toggle
+
+var toggle = document.querySelector('#toggle');
+var menu = document.querySelector('#menu');
+menu.classList.remove('is-active')
+var menuItems = document.querySelectorAll('#menu li a');
+
+toggle.addEventListener('click', function(){
+  if (menu.classList.contains('is-active')) {
+    this.setAttribute('aria-expanded', 'false');
+    menu.classList.remove('is-active');
+  } else {
+    menu.classList.add('is-active');
+    this.setAttribute('aria-expanded', 'true');
+    menuItems[0].focus();
+  }
+});
